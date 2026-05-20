e6512c3b5c3acb560576317ac6767c352ec7b4c6
anonyfriday <duykimvu75@gmail.com>
2026-05-21 01:45:31 +0700

    setup(fe): add GoBackButton

---
+++ Frontend/features/shared/components/GoBackButton.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+type GoBackButtonProps = {
+  href: string;
+  title: string;
+};
+
+export default function GoBackButton({ href, title }: GoBackButtonProps) {
+  return (
+    <div className="flex items-center gap-2">
+      <Link href={href} className="btn btn-primary btn-circle btn-ghost">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+        </svg>
+      </Link>
+      <h1 className="text-2xl font-bold text-primary">{title}</h1>
+    </div>
+  );
+}
