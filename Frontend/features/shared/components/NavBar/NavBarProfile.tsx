4d34ffbd5cf8b024a36ec57908c3cf51e650c182
anonyfriday <duykimvu75@gmail.com>
2026-05-21 02:54:35 +0700

    setup(fe): add NavBar Profile

---
+++ Frontend/features/shared/components/NavBar/NavBarProfile.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+export default function NavbarProfile() {
+  const handleSignOut = () => {
+    // will add sign out later
+    console.log("Sign out clicked");
+  };
+
+  return (
+    <div className="dropdown dropdown-end">
+      <div
+        tabIndex={0}
+        role="button"
+        className="btn btn-ghost text-white text-base font-semibold normal-case flex items-center gap-1 hover:bg-white/10"
+      >
+        <span>binhnv</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2.5}
+          stroke="currentColor"
+          className="w-4 h-4"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m19.5 8.25-7.5 7.5-7.5-7.5"
+          />
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="dropdown-content menu bg-base-100 rounded-box z-[100] w-40 p-2 shadow text-base-content mt-2 border border-base-200"
+      >
+        <li>
+          <button
+            onClick={handleSignOut}
+            className="text-error font-semibold hover:bg-error/10 active:bg-error/20 w-full text-left"
+          >
+            Sign out
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
