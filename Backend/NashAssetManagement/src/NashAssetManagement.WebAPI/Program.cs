b64f18d5efd6be133ce2d48e2d4bf71fa0585541
Nguyễn Thái Hòa <hoa41300@gmail.com>
2026-05-21 18:04:43 +0700

    setup(be): Setup logging
    - Setup application logging with Serilog.
    - Configure Serilog to log to console, text and json sinks.

--- Backend/NashAssetManagement/src/NashAssetManagement.WebAPI/Program.cs
+++ Backend/NashAssetManagement/src/NashAssetManagement.WebAPI/Program.cs
@@ -2,27 +2,47 @@ using NashAssetManagement.Application;
 using NashAssetManagement.Infrastructure;
 using NashAssetManagement.Persistence;
 using NashAssetManagement.WebAPI;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-    .AddApplicationServices(builder.Configuration)
-    .AddPersistenceServices(builder.Configuration)
-    .AddInfrastructureServices(builder.Configuration)
-    .AddApiServices(builder.Configuration);
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+builder.Host.UseSerilog();
 
-
-var app = builder.Build();
-
-app.UseExceptionHandler();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
+    Log.Information("Starting application");
+    builder.Services
+        .AddApplicationServices(builder.Configuration)
+        .AddPersistenceServices(builder.Configuration)
+        .AddInfrastructureServices(builder.Configuration)
+        .AddApiServices(builder.Configuration);
+
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    app.UseExceptionHandler();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
