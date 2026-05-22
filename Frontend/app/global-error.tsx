46b5496196ceedb272d0cd63dd6072237e4f95c8
anonyfriday <duykimvu75@gmail.com>
2026-05-23 02:47:30 +0700

    fix(login): add user check on global-error.ts (#5199)

--- Frontend/app/global-error.tsx
+++ Frontend/app/global-error.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
+import Link from "next/link";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { UserRoles } from "@/features/users/users.types";
