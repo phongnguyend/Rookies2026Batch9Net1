faa29338ae756e9d38c5ee1ba932a32edbef95bc
Milod <ngxdung172.work@gmail.com>
2026-05-22 17:39:15 +0700

    setup(be): add data seeder for table Asset, Assignment and Return Request

---
+++ Backend/NashAssetManagement/src/NashAssetManagement.Persistence/Builder/AssignmentBuilder.cs
@@ -0,0 +1,96 @@
+﻿using NashAssetManagement.Domain.Entities.Core;
+using NashAssetManagement.Domain.Enums;
+
+namespace NashAssetManagement.Persistence.Builder
+{
+    public sealed class AssignmentBuilder
+    {
+        private Guid _id = Guid.NewGuid();
+        private Guid _assetId;
+        private Guid _assignedToUserId;
+        private Guid _assignedByUserId;
+        private DateTime _assignedDateAtUtc = DateTime.UtcNow;
+        private string _note = "Note Sample";
+        private AssignmentState _state = AssignmentState.WaitingForAcceptance;
+        private bool _isReturning = false;
+        private DateTime _createdAtUtc = DateTime.UtcNow;
+        private DateTime? _updatedAtUtc;
+
+        public AssignmentBuilder WithId(Guid id)
+        {
+            _id = id;
+            return this;
+        }
+
+        public AssignmentBuilder WithAssetId(Guid assetId)
+        {
+            _assetId = assetId;
+            return this;
+        }
+
+        public AssignmentBuilder WithAssignedToUserId(Guid assignedToUserId)
+        {
+            _assignedToUserId = assignedToUserId;
+            return this;
+        }
+
+        public AssignmentBuilder WithAssignedByUserId(Guid assignedByUserId)
+        {
+            _assignedByUserId = assignedByUserId;
+            return this;
+        }
+
+        public AssignmentBuilder WithAssignedDateAtUtc(DateTime assignedDateAtUtc)
+        {
+            _assignedDateAtUtc = assignedDateAtUtc;
+            return this;
+        }
+
+        public AssignmentBuilder WithNote(string note)
+        {
+            _note = note;
+            return this;
+        }
+
+        public AssignmentBuilder WithState(AssignmentState state)
+        {
+            _state = state;
+            return this;
+        }
+
+        public AssignmentBuilder WithIsReturning(bool isReturning)
+        {
+            _isReturning = isReturning;
+            return this;
+        }
+
+        public AssignmentBuilder WithCreatedAtUtc(DateTime createdAtUtc)
+        {
+            _createdAtUtc = createdAtUtc;
+            return this;
+        }
+
+        public AssignmentBuilder WithUpdatedAtUtc(DateTime? updatedAtUtc)
+        {
+            _updatedAtUtc = updatedAtUtc;
+            return this;
+        }
+
+        public Assignment Build()
+        {
+            return new Assignment
+            {
+                Id = _id,
+                AssetId = _assetId,
+                AssignedToUserId = _assignedToUserId,
+                AssignedByUserId = _assignedByUserId,
+                AssignedAtUtc= _assignedDateAtUtc,
+                Note = _note,
+                State = _state,
+                IsReturning = _isReturning,
+                CreatedAtUtc = _createdAtUtc,
+                UpdatedAtUtc = _updatedAtUtc,
+            };
+        }
+    }
+}
---
+++ Backend/NashAssetManagement/src/NashAssetManagement.Persistence/Builder/ReturnRequestBuilder.cs
@@ -0,0 +1,80 @@
+﻿using NashAssetManagement.Domain.Entities.Core;
+using NashAssetManagement.Domain.Enums;
+
+namespace NashAssetManagement.Persistence.Builder
+{
+    public sealed class ReturnRequestBuilder
+    {
+        private Guid _id = Guid.NewGuid();
+        private DateTime? _returnedAtUtc;
+        private ReturnRequestState _state = ReturnRequestState.WaitingForReturning;
+        private Guid _assignmentId = Guid.NewGuid();
+        private Guid _requestedByUserId = Guid.NewGuid();
+        private Guid? _acceptedByUserId;
+        private DateTime _createdAtUtc = DateTime.UtcNow;
+        private DateTime? _updatedAtUtc;
+
+        public ReturnRequestBuilder WithId(Guid id)
+        {
+            _id = id;
+            return this;
+        }
+
+        public ReturnRequestBuilder WithReturnedAtUtc(DateTime? returnedAtUtc)
+        {
+            _returnedAtUtc = returnedAtUtc;
+            return this;
+        }
+
+        public ReturnRequestBuilder WithState(ReturnRequestState state)
+        {
+            _state = state;
+            return this;
+        }
+
+        public ReturnRequestBuilder WithAssignmentId(Guid assignmentId)
+        {
+            _assignmentId = assignmentId;
+            return this;
+        }
+
+        public ReturnRequestBuilder WithRequestedByUserId(Guid requestedByUserId)
+        {
+            _requestedByUserId = requestedByUserId;
+            return this;
+        }
+
+        public ReturnRequestBuilder WithAcceptedByUserId(Guid? acceptedByUserId)
+        {
+            _acceptedByUserId = acceptedByUserId;
+            return this;
+        }
+
+        public ReturnRequestBuilder WithCreatedAtUtc(DateTime createdAtUtc)
+        {
+            _createdAtUtc = createdAtUtc;
+            return this;
+        }
+
+        public ReturnRequestBuilder WithUpdatedAtUtc(DateTime? updatedAtUtc)
+        {
+            _updatedAtUtc = updatedAtUtc;
+            return this;
+        }
+
+        public ReturnRequest Build()
+        {
+            return new ReturnRequest
+            {
+                Id = _id,
+                ReturnedAtUtc = _returnedAtUtc,
+                State = _state,
+                AssignmentId = _assignmentId,
+                RequestedByUserId = _requestedByUserId,
+                AcceptedByUserId = _acceptedByUserId,
+                CreatedAtUtc = _createdAtUtc,
+                UpdatedAtUtc = _updatedAtUtc,
+            };
+        }
+    }
+}
