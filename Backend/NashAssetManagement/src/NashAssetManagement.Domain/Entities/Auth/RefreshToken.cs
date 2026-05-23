3ae2c2dcc045777752e17c02d352f397586fbf3c
anonyfriday <duykimvu75@gmail.com>
2026-05-24 01:38:19 +0700

    feat(auth): add RefreshToken entity(#5199)

---
+++ Backend/NashAssetManagement/src/NashAssetManagement.Domain/Entities/Auth/RefreshToken.cs
@@ -0,0 +1,14 @@
+using NashAssetManagement.Domain.Entities.Base;
+
+namespace NashAssetManagement.Domain.Entities.Auth
+{
+    public class RefreshToken : BaseEntity<Guid>, IRevokable, IExpirable
+    {
+        public Guid UserId { get; set; }
+        public string Token { get; set; } = default!;
+        public DateTime CreatedAtUtc { get; set; }
+        public DateTime ExpiresAtUtc { get; set; }
+        public bool IsRevoked { get; set; }
+        public DateTime? RevokedAtUtc { get; set; }
+    }
+}
