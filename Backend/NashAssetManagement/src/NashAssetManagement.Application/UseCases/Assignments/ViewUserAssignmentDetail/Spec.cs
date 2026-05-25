11c127f8ba21c01c693d53bafc8d6dce15f0e3c5
Nguyễn Thái Hòa <hoa41300@gmail.com>
2026-05-25 16:18:20 +0700

    feat(be): Add View user's assignment detail endpoint

---
+++ Backend/NashAssetManagement/src/NashAssetManagement.Application/UseCases/Assignments/ViewUserAssignmentDetail/Spec.cs
@@ -0,0 +1,25 @@
+﻿using Ardalis.Specification;
+using NashAssetManagement.Domain.Entities.Core;
+
+namespace NashAssetManagement.Application.UseCases.Assignments.ViewUserAssignmentDetail
+{
+    internal class Spec
+        : Specification<Assignment, Response>
+    {
+        public Spec(Guid assignmentId, Guid assigneeId)
+        {
+            Query.Where(x => x.Id == assignmentId && x.AssignedToUserId == assigneeId)
+                .AsNoTracking()
+                .Select(x => new Response(
+                    x.Id,
+                    x.Asset!.AssetCode,
+                    x.Asset.Name,
+                    x.Asset.Specification,
+                    x.AssignedByUser!.UserName!,
+                    x.AssignedToUser!.UserName!,
+                    x.CreatedAtUtc,
+                    x.State.ToString(),
+                    x.Note));
+        }
+    }
+}
