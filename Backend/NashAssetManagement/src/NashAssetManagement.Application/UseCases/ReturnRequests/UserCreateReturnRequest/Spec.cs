5e76ce3ada1800d59225753cfe25c1c03feccf8e
Nguyễn Thái Hòa <hoa41300@gmail.com>
2026-05-26 13:54:42 +0700

    feat(be): Add endpoint to Create return request for user's assignment

---
+++ Backend/NashAssetManagement/src/NashAssetManagement.Application/UseCases/ReturnRequests/UserCreateReturnRequest/Spec.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.Specification;
+using NashAssetManagement.Domain.Entities.Core;
+
+namespace NashAssetManagement.Application.UseCases.ReturnRequests.UserCreateReturnRequest
+{
+    internal class Spec
+        : Specification<Assignment>
+    {
+        public Spec(Guid assignmentId)
+        {
+            Query.Where(x => x.Id == assignmentId)
+                .Include(x => x.ReturnRequests);
+        }
+    }
+}
