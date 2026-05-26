e66d29c8c3b1a3a23ef921c6a33a119c362b8ba8
Milod <ngxdung172.work@gmail.com>
2026-05-26 21:04:27 +0700

    fix delete wrong folder

--- Backend/NashAssetManagement/src/NashAssetManagement.Application/UseCases/Assignments/GetAll/PagedQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NashAssetManagement.Application.UseCases.Assignments.GetAll
-{
-    public record PagedQuery
-    {
-        public int? PageNumber { get; init; } = 1;
-        public int? PageSize { get; init; } = 10;
-    }
-}
--- Backend/NashAssetManagement/src/NashAssetManagement.Application/UseCases/Assignments/GetById/Query.cs
+++
@@ -1,7 +0,0 @@
-﻿using ErrorOr;
-using MediatR;
-
-namespace NashAssetManagement.Application.UseCases.Assignments.GetById
-{
-    public record Query(Guid Id): IRequest<ErrorOr<Response>>;
-}
