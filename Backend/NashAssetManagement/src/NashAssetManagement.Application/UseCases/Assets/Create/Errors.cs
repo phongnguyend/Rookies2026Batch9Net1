0d8c40d5f708592de7e7eec3ac0f026c76210bdd
HUYPHOENIX <truongquanghuy2003@gmail.com>
2026-05-26 22:17:58 +0700

    chore: clean up

--- Backend/NashAssetManagement/src/NashAssetManagement.Application/UseCases/Assets/Create/Errors.cs
+++
@@ -1,9 +0,0 @@
-using ErrorOr;
-
-namespace NashAssetManagement.Application.UseCases.Assets.Create;
-
-public static class CreateAssetError
-{
-    public static Error NotFound(Guid id) =>
-        Error.NotFound("Asset.NotFound", $"Asset with id '{id}' was not found.");
-}
