416ea6780b9dfde0b6af8c173dd0b5a608a08ec7
Nguyễn Thái Hòa <91940248+HoaNT3010@users.noreply.github.com>
2026-05-27 15:19:59 +0700

    feat: Create returning request for user's assignment (#5213) (#31)
    Summary
    - Add create returning request for user's assignment feature
    
    Changes
    - Create backend API endpoints for user to create return request for their own assignment.
    - Create unit tests for API components.
    - Create UI for the features in the frontend application.
    - Integrate backend APIs to frontend.
    
    Acceptance Criteria
    - AC1 from ticket (#5213)
    - AC3 from ticket (#5213)
    - AC3 from ticket (#5213)
    - Design from ticket (#5264)

---
+++ Backend/NashAssetManagement/src/NashAssetManagement.Application/UseCases/ReturnRequests/UserCreateReturnRequest/Errors.cs
@@ -0,0 +1,42 @@
+﻿using ErrorOr;
+
+namespace NashAssetManagement.Application.UseCases.ReturnRequests.UserCreateReturnRequest
+{
+    internal static class Errors
+    {
+        public static Error UnauthorizedUser =
+            Error.Unauthorized(
+                "UserCreateReturnRequest.UnauthorizedUser",
+                "User is not authorized to do this action.");
+
+        public static Error UnidentifiedUser =
+            Error.Unauthorized(
+                "UserCreateReturnRequest.UnidentifiedUser",
+                "Cannot identify user with user's ID.");
+
+        public static Error AssignmentNotFoundWithId(string assignmentId) =>
+            Error.NotFound(
+                "UserCreateReturnRequest.AssignmentNotFoundWithId",
+                $"Failed to find assignment with Id '{assignmentId}'.");
+
+        public static Error AssignmentNotAssignedToUser =
+            Error.Forbidden(
+                "UserCreateReturnRequest.AssignmentNotAssignedToUser",
+                "User can only create return request for asset assigned to them.");
+
+        public static Error AssignmentHasWaitingReturnRequest =
+            Error.Conflict(
+                "UserCreateReturnRequest.AssignmentHasWaitingReturnRequest",
+                "A pending return request has been created for this assignment.");
+
+        public static Error UnexpectedErrorOccurred =
+            Error.Failure(
+                "UserCreateReturnRequest.UnexpectedErrorOccurred",
+                "Unexpected error occurred when trying to create return request for user's assignment. Please try again.");
+
+        public static Error InvalidAssignmentState =
+            Error.Validation(
+                "UserCreateReturnRequest.InvalidAssignmentState",
+                "Assignment must be in 'Accepted' state in order to create return request.");
+    }
+}
