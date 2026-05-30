3a7db132eae94811e03889e44d69226acf834847
Nguyễn Thái Hòa <hoa41300@gmail.com>
2026-05-30 16:45:56 +0700

    refactor: Update View asset detail usecase's errors.

--- Backend/NashAssetManagement/src/NashAssetManagement.Application/UseCases/Assets/ViewDetail/Errors.cs
+++ Backend/NashAssetManagement/src/NashAssetManagement.Application/UseCases/Assets/ViewDetail/Errors.cs
@@ -5,12 +5,12 @@ namespace NashAssetManagement.Application.UseCases.Assets.ViewDetail;
 public static class GetAssetDetailErrors
 {
     public static readonly Error NotFoundAssetId = Error.NotFound(
-        code:"GetAssetDetail.AssetIsNotFound",
-        description:"GetAssetDetail.AssetNotFound"
+        code: "GetAssetDetail.NotFoundAssetId",
+        description:"Asset with given ID was not found."
     );
 
     public static readonly Error NotFoundLocation = Error.NotFound(
-        code:"GetAssetDetail.LocationIsNotFound",
-        description: "GetAssetDetail.LocationNotFound"
+        code: "GetAssetDetail.NotFoundLocation",
+        description: "Asset's location was not found."
     );
 }
