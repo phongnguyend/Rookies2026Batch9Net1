503337e5f4c5743aa47bd07d8aaaede4890f5ce0
HUYPHOENIX <truongquanghuy2003@gmail.com>
2026-06-01 21:50:37 +0700

    feat: add Api for backend

---
+++ Backend/NashAssetManagement/src/NashAssetManagement.WebAPI/Controllers/Assets/EditAssetController.cs
@@ -0,0 +1,38 @@
+using Asp.Versioning;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NashAssetManagement.Application.UseCases.Assets.Edit;
+using NashAssetManagement.Domain.Constants;
+using NashAssetManagement.WebAPI.Utilities;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace NashAssetManagement.WebAPI.Controllers.Assets;
+
+[ApiVersion(1)]
+[Authorize(Roles = ApplicationRole.Admin)]
+[Route("api/v{version:apiVersion}/assets")]
+public class EditAssetController : BaseApiController
+{
+    public EditAssetController(ISender sender) : base(sender) { }
+
+    [HttpPost]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status409Conflict)]
+    [SwaggerOperation(Summary = "Create a new asset.",Tags = [ControllerTags.Assets])]
+    public async Task<IActionResult> Create(
+        [FromBody] EditAssetRequest request,
+        CancellationToken cancellationToken)
+    {
+        var result = await _sender.Send(request, cancellationToken);
+
+        return result.Match(
+            asset => Ok(asset),
+            errors => errors.ToProblem()
+        );
+    }
+}
