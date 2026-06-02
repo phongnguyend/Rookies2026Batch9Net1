bb9b3346d67063a85d7a341e552fec9cdaa9cb21
Nguyễn Thái Hòa <hoa41300@gmail.com>
2026-06-02 14:13:43 +0700

    feat(be): Add endpoint to lookup asset

---
+++ Backend/NashAssetManagement/src/NashAssetManagement.WebAPI/Controllers/Assets/AssetsLookupController.cs
@@ -0,0 +1,44 @@
+﻿using Asp.Versioning;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NashAssetManagement.Application.UseCases.Assets.AssetsLookup;
+using NashAssetManagement.Application.Utilities;
+using NashAssetManagement.Domain.Constants;
+using NashAssetManagement.WebAPI.Utilities;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace NashAssetManagement.WebAPI.Controllers.Assets
+{
+    [ApiVersion(1)]
+    [Route("api/v{version:apiVersion}/assets")]
+    public class AssetsLookupController
+        : BaseApiController
+    {
+        public AssetsLookupController(ISender sender) : base(sender)
+        {
+        }
+
+        [HttpGet("lookup")]
+        [Authorize(Roles = ApplicationRole.Admin)]
+        [ProducesResponseType(typeof(PagedList<Response>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [SwaggerOperation(
+            Summary = "Lookup assets to assign to user.",
+            Description = "Allow admin to lookup assets that available for assigning. Required admin to be authenticated.",
+            Tags = [ControllerTags.Assets])]
+        public async Task<IActionResult> AssetsLookup(
+            [FromQuery] Request request,
+            CancellationToken cancellationToken)
+        {
+            var result = await _sender.Send(request, cancellationToken);
+            return result.Match(
+                Ok,
+                errors => errors.ToProblem());
+        }
+    }
+}
