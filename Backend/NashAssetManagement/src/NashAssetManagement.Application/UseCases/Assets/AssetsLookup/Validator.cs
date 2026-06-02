7f816b5c2eee23f607d93eb2bca89300fed897f3
Nguyễn Thái Hòa <hoa41300@gmail.com>
2026-06-02 10:05:11 +0700

    feat(be): Add usecase to Lookup users for manage assignment

---
+++ Backend/NashAssetManagement/src/NashAssetManagement.Application/UseCases/Assets/AssetsLookup/Validator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using NashAssetManagement.Application.Common.Validators;
+
+namespace NashAssetManagement.Application.UseCases.Assets.AssetsLookup
+{
+    internal class Validator
+        : AbstractValidator<Request>
+    {
+        public Validator()
+        {
+            RuleFor(x => x.SearchTerm)
+                .MaximumLength(100)
+                .WithMessage("Search term cannot exceed 100 characters.")
+                .When(x => !string.IsNullOrWhiteSpace(x.SearchTerm));
+
+            RuleFor(x => x.PageNumber)
+               .MustBeValidPageNumber();
+
+            RuleFor(x => x.PageSize)
+                .MustBeValidPageSize();
+        }
+    }
+}
