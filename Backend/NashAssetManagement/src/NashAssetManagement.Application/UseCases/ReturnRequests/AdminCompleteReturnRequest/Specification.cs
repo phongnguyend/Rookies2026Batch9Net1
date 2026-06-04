4c8eae74084208b94453ee7d3af9e3e8b6726304
Huy Nguyễn <huynvtse171283@fpt.edu.vn>
2026-06-04 16:30:20 +0700

    feat(complete-return-request): complete return request and update assignment/asset states (#5211)

---
+++ Backend/NashAssetManagement/src/NashAssetManagement.Application/UseCases/ReturnRequests/AdminCompleteReturnRequest/Specification.cs
@@ -0,0 +1,16 @@
+using Ardalis.Specification;
+using NashAssetManagement.Domain.Entities.Core;
+
+namespace NashAssetManagement.Application.UseCases.ReturnRequests.AdminCompleteReturnRequest
+{
+    internal class Specification
+        : Specification<ReturnRequest>
+    {
+        public Specification(Guid returnRequestId)
+        {
+            Query.Where(x => x.Id == returnRequestId)
+                .Include(x => x.Assignment)
+                .ThenInclude(x => x.Asset);
+        }
+    }
+}
