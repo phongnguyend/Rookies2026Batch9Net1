abe6a34b3f5036e36deeeae4b7a14d9888b84150
HUYPHOENIX <truongquanghuy2003@gmail.com>
2026-06-05 07:12:11 +0700

    feat: add Unit Test for delete asset

---
+++ Backend/NashAssetManagement/tests/NashAssetManagement.UnitTests/Application/UseCases/Assets/Delete/ValidatorTest.cs
@@ -0,0 +1,51 @@
+using FluentValidation.TestHelper;
+using Xunit;
+using NashAssetManagement.Application.UseCases.Assets.Delete;
+
+public class DeleteAssetValidatorTests
+{
+    private readonly DeleteAssetValidator _validator = new();
+
+    [Fact]
+    public void Validate_EmptyId_ShouldReturnError()
+    {
+        var request = new DeleteAssetRequest("");
+
+        var result = _validator.TestValidate(request);
+
+        result.ShouldHaveValidationErrorFor(x => x.Id)
+              .WithErrorMessage("Asset id is required");
+    }
+
+    [Fact]
+    public void Validate_NullId_ShouldReturnError()
+    {
+        var request = new DeleteAssetRequest(null!);
+
+        var result = _validator.TestValidate(request);
+
+        result.ShouldHaveValidationErrorFor(x => x.Id)
+              .WithErrorMessage("Asset id is required");
+    }
+
+    [Fact]
+    public void Validate_InvalidGuid_ShouldReturnGuidFormatError()
+    {
+        var request = new DeleteAssetRequest("invalid-guid");
+
+        var result = _validator.TestValidate(request);
+
+        result.ShouldHaveValidationErrorFor(x => x.Id)
+              .WithErrorMessage("Asset id must be in Guid Format.");
+    }
+
+    [Fact]
+    public void Validate_ValidGuid_ShouldPass()
+    {
+        var request = new DeleteAssetRequest(Guid.NewGuid().ToString());
+
+        var result = _validator.TestValidate(request);
+
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
