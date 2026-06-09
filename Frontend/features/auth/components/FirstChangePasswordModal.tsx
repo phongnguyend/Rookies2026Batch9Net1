76473c24943ff53d473c68e2bf6a15b05ead6b44
An0nyFr1day <56812770+AnonyFriday@users.noreply.github.com>
2026-06-09 16:07:38 +0700

    fix: fix default revealed password on MS (#132)

--- Frontend/features/auth/components/FirstChangePasswordModal.tsx
+++ Frontend/features/auth/components/FirstChangePasswordModal.tsx
@@ -199,29 +199,36 @@ export default function FirstChangePasswordModal({
                         {showNew ? (
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="w-4 h-4"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-5 h-5"
                           >
                             <path
-                              fillRule="evenodd"
-                              d="M3.28 2.22a.75.75 0 00-1.06 1.06l14.5 14.5a.75.75 0 101.06-1.06L3.28 2.22zM8.14 5.2a7.5 7.5 0 017.58 3.5 7.21 7.21 0 01-.68 1.05l-1.12-1.12a5.75 5.75 0 00-5.78-4.23l-.68-.68c.22-.01.44-.02.68-.02zM12.92 10l-1.92-1.92a2.25 2.25 0 00-2.25 2.25l1.92 1.92a2.25 2.25 0 002.25-2.25z"
-                              clipRule="evenodd"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
                             />
-                            <path d="M10 14a4.002 4.002 0 003.58-2.215l-1.282-1.282a2.5 2.5 0 01-4.596-1.03l-1.503-1.503A7.514 7.514 0 002.5 8.7a7.5 7.5 0 007.5 5.3z" />
                           </svg>
                         ) : (
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="w-4 h-4"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-5 h-5"
                           >
-                            <path d="M10 12.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z" />
                             <path
-                              fillRule="evenodd"
-                              d="M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.08.21.08.44 0 .65A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                              clipRule="evenodd"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                            />
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                             />
                           </svg>
                         )}
@@ -263,29 +270,36 @@ export default function FirstChangePasswordModal({
                         {showConfirm ? (
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="w-4 h-4"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-5 h-5"
                           >
                             <path
-                              fillRule="evenodd"
-                              d="M3.28 2.22a.75.75 0 00-1.06 1.06l14.5 14.5a.75.75 0 101.06-1.06L3.28 2.22zM8.14 5.2a7.5 7.5 0 017.58 3.5 7.21 7.21 0 01-.68 1.05l-1.12-1.12a5.75 5.75 0 00-5.78-4.23l-.68-.68c.22-.01.44-.02.68-.02zM12.92 10l-1.92-1.92a2.25 2.25 0 00-2.25 2.25l1.92 1.92a2.25 2.25 0 002.25-2.25z"
-                              clipRule="evenodd"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
                             />
-                            <path d="M10 14a4.002 4.002 0 003.58-2.215l-1.282-1.282a2.5 2.5 0 01-4.596-1.03l-1.503-1.503A7.514 7.514 0 002.5 8.7a7.5 7.5 0 007.5 5.3z" />
                           </svg>
                         ) : (
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="w-4 h-4"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-5 h-5"
                           >
-                            <path d="M10 12.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z" />
                             <path
-                              fillRule="evenodd"
-                              d="M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.08.21.08.44 0 .65A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                              clipRule="evenodd"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                            />
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                             />
                           </svg>
                         )}
